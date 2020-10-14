4fddc64ba8abf095f837870a4923e03ed95135bd
jbericat <jbericat@uoc.edu>
2020-10-14 23:59:39 +0200

    v6B
    Disseny UML Original (Company<->Agreements)

--- scripts/4.CREATE-TABLES.sql
+++ scripts/4.CREATE-TABLES.sql
@@ -1,40 +1,32 @@
-CREATE TABLE companies OF Company_ob (PRIMARY KEY (CIF)) 
-	NESTED TABLE hasAgreements STORE AS hasAgreements_nt(
-	(PRIMARY KEY (NESTED_TABLE_ID))
-		NESTED TABLE hasAddendums STORE AS hasAddendums_nt); 
-
+CREATE TABLE companies OF Company_ob (CIF PRIMARY KEY) 
+  NESTED TABLE hasAgreements STORE AS hasAgreements_nt
+   (NESTED TABLE hasAddendums STORE AS hasAddendums_nt); 
 /
 
 /* CREATE TABLE agreementCols OF AgreementCol_ob (
 		CHECK (hasLinesOfResearch IS NOT NULL),
 		CHECK (hasStakeholder IS NOT NULL)
 		);
-
 -- AQUESTS CHECKS HAN D'ANAR AL NESTED TABLE hasAgreements_nt
-
 / */
 
--- agreementInts no compila, possiblement per culpa de les nested tables multicapa. La solució es canviar una NT per un VARRAY gran (1000 per ex.) o unscoped:
--- https://www.experts-exchange.com/questions/21344386/Oracle-problem-creating-multi-layer-nested-table.html
-
-CREATE TABLE PDIS OF PDI_ob (
-        PRIMARY KEY (NIF),
-        CHECK (hasSpecialty IS NOT NULL)
-        --OBJECT IDENTIFIER IS PRIMARY KEY
-        ); 
-
+CREATE TABLE PDIS OF PDI_ob (NIF PRIMARY KEY)
+  OBJECT IDENTIFIER IS PRIMARY KEY; 
 /
 
-CREATE TABLE students OF Student_ob (PRIMARY KEY (NIF));
-
+ALTER TABLE PDIS
+  ADD CONSTRAINT hasSpecialty CHECK (hasSpecialty IS NOT NULL);
 /
 
-CREATE TABLE staff OF Staff_ob (PRIMARY KEY (NIF))
---OBJECT IDENTIFIER IS PRIMARY KEY
-;
-
+CREATE TABLE students OF Student_ob (NIF PRIMARY KEY)
+  OBJECT IDENTIFIER IS PRIMARY KEY;
 /
 
-CREATE TABLE lResearches OF LResearch_ob (PRIMARY KEY (name));
+CREATE TABLE staff OF Staff_ob (NIF PRIMARY KEY)
+  OBJECT IDENTIFIER IS PRIMARY KEY; 
+/
 
+CREATE TABLE lResearches OF LResearch_ob (name PRIMARY KEY);
+  /* Si ho activem passa el següent al inserir files a PDIS -> Error report -ORA-22979: cannot INSERT object view REF or user-defined REF */
+  --OBJECT IDENTIFIER IS PRIMARY KEY; 
 /
--- scripts/5.INSERT-ROWS.sql
+++ scripts/5.INSERT-ROWS.sql
@@ -1,102 +1,102 @@
 INSERT INTO staff 
 VALUES ('11111111C',
-        fullname('Elon','Reeve','Musk')
+  fullname('Elon','Reeve','Musk')
 );
 
 INSERT INTO staff 
 VALUES ('2222222C',
-        fullname('MacKenzie','Sheri','Tuttle')
-        );
+  fullname('MacKenzie','Sheri','Tuttle')
+);
 
 INSERT INTO students 
 VALUES ('11111111A',
-        111111,
-        fullname('John','Winston','Lenon'),
-        currentStudiesList('PHILOSOPHY','CONTEMPORARY ARTS')
+  111111,
+  fullname('John','Winston','Lenon'),
+  currentStudiesList('PHILOSOPHY','CONTEMPORARY ARTS')
 );
 
 INSERT INTO students 
 VALUES ('2222222A',
-        222222,
-        fullname('Bob','Nesta','Marley'),
-        currentStudiesList('BOTANICS','BIOENGINEERING','BIOLOGY')
+  222222,
+  fullname('Bob','Nesta','Marley'),
+  currentStudiesList('BOTANICS','BIOENGINEERING','BIOLOGY')
 );
 
 INSERT INTO lresearches
 VALUES ('ASTROPHYSICS',
-        'Study of physics from a cosmological perspective',
-        '1-January-2020',
+  'Study of physics from a cosmological perspective',
+  '1-January-2020',
 		'1-January-2030'
 );
 
 INSERT INTO lresearches
 VALUES ('RADIOACTIVITY',
-        'Study of nuclear radiation and its properties',
-        '1-January-2020',
+  'Study of nuclear radiation and its properties',
+  '1-January-2020',
 		'1-January-2030'
 );
 
 INSERT INTO lresearches
 VALUES ('COMPUTER ARCHITECTURES',
-        'Computer hardware design and performance analysis',
-        '1-January-2020',
+  'Computer hardware design and performance analysis',
+  '1-January-2020',
 		'1-January-2030'
 );
 
 INSERT INTO lresearches
 VALUES ('SOFTWARE ENGINEERING',
-        'Computer software modeling, design and implementation',
-        '1-January-2020',
+  'Computer software modeling, design and implementation',
+  '1-January-2020',
 		'1-January-2030'
 );
 
 INSERT INTO lresearches
 VALUES ('QUANTUM PHYSICS',
-        'Study of the physical properties of nature at the scale of atoms and subatomic particles',
-        '1-January-2020',
+  'Study of the physical properties of nature at the scale of atoms and subatomic particles',
+  '1-January-2020',
 		'1-January-2030'
 );
 
 INSERT INTO lresearches
 VALUES ('ENGINEERING',
-        ' use of scientific principles to design and build machines',
-        '1-January-2020',
+  ' use of scientific principles to design and build machines',
+  '1-January-2020',
 		'1-January-2030'
 );
 
 
 INSERT INTO PDIS 
 VALUES ('11111111B',
-        333333,
-        fullname('Mileva','Marić',''),
-        'PHYSICS',
-        '19-December-1875',
-        (select ref(d) from lResearches d where d.name='ASTROPHYSICS') 
+  333333,
+  fullname('Mileva','Marić',''),
+  'PHYSICS',
+  '19-December-1875',
+  (select ref(d) from lResearches d where d.name='ASTROPHYSICS') 
 );
 
 INSERT INTO PDIS 
 VALUES ('22222222B',
-        444444,
-        fullname('Marie','Skłodowska','Curie'),
-        'PHYSICS',
-        '7-November-1867',
-        (select ref(d) from lResearches d where name='NUCLEAR FUSSION') --THE NOT NULL CLAUSE ENFORCES THE REFERENTIAL INTEGRITY CONSTRAINT (MULTIPLICITY OF "1" IN THE UML DIAGRAM)
+  444444,
+  fullname('Marie','Skłodowska','Curie'),
+  'PHYSICS',
+  '7-November-1867',
+  (select ref(d) from lResearches d where name='NUCLEAR FUSSION') --THE NOT NULL CLAUSE ENFORCES THE REFERENTIAL INTEGRITY CONSTRAINT (MULTIPLICITY OF "1" IN THE UML DIAGRAM)
 );
 
 INSERT INTO PDIS 
 VALUES ('22222222B',
-        22222,
-        fullname('Marie','Sklodowska','Curie'),
-        'PHYSICS',
-        '7-November-1867',
-        (select ref(r) from lResearches r where name='RADIOACTIVITY') 
+  22222,
+  fullname('Marie','Sklodowska','Curie'),
+  'PHYSICS',
+  '7-November-1867',
+  (select ref(r) from lResearches r where name='RADIOACTIVITY') 
 );
 
 INSERT INTO companies (CIF,businessName,postalCode,sector) -- THIS WAY THE hasAgreement_tab table constructor is automatically called (p.32 M2)
 VALUES ('Z12345678',
-        'IBM',
-        08080,
-        'COMPUTING'
+  'IBM',
+  08080,
+  'COMPUTING'
 );
 
 /* INSERIR A LA NESTED TABLE hasAgreements_nt FILES NOVES D'UN SUBTIPUS D'AQUESTA: 
@@ -104,49 +104,49 @@ INSERIM FILES DE TIPUS AGREEMENTCOL (SUBCLASSE) EN UNA TAULA DE TIPUS AGREEMENT 
 
 INSERT INTO TABLE (SELECT c.hasAgreements FROM companies c WHERE c.businessname like 'IBM') 
 VALUES (AgreementCol_ob (
-        '1-January-2020', 
-        '1-January-2045',
-        'QUANTUM COMPUTING RESEARCH COLABORATION', 
+  '1-January-2020', 
+  '1-January-2045',
+  'QUANTUM COMPUTING RESEARCH COLABORATION', 
 		'N',
 		(select ref(s) from PDIS s where s.NIF='22222222B'),
-        refLResearch_va((select ref(r1) from LResearches r1 where r1.name='QUANTUM PHYSICS'),
-		             (select ref(r2) from LResearches r2 where r2.name='ENGINEERING'),
+  refLResearch_va((select ref(r1) from LResearches r1 where r1.name='QUANTUM PHYSICS'),
+		       (select ref(r2) from LResearches r2 where r2.name='ENGINEERING'),
 					 (select ref(r2) from LResearches r2 where r2.name='COMPUTER ARCHITECTURES')
-        ))
+  ))
 );
 
 INSERT INTO TABLE (SELECT c.hasAgreements FROM companies c WHERE c.businessname like 'IBM') 
 VALUES (AgreementCol_ob (
-        '1-January-2020', 
-        '1-January-2025', 
-        'BLACK HOLE SIMULATOR PROJECT', 
+  '1-January-2020', 
+  '1-January-2025', 
+  'BLACK HOLE SIMULATOR PROJECT', 
 		'Y',
 		(select ref(s) from PDIS s where s.NIF='11111111B'),
-        refLResearch_va((select ref(r1) from LResearches r1 where r1.name='SOFTWARE ENGINEERING'),
-		             (select ref(r2) from LResearches r2 where r2.name='ASTROPHYSICS')
-        ))
+  refLResearch_va((select ref(r1) from LResearches r1 where r1.name='SOFTWARE ENGINEERING'),
+		       (select ref(r2) from LResearches r2 where r2.name='ASTROPHYSICS')
+  ))
 );
 
 /* SI VOLEM ACTUALITZAR UNA FILERA D'UN NESTED TABLE PROCEDIM COM SEGUEIX */
 
 /*UPDATE companies 
 SET hasAgreements = Agreement_tab ( AgreementCol_ob (
-        '1-January-2020', 
-        '1-January-2025', 
-        'BLACK HOLE SIMULATOR PROJECT', 
+  '1-January-2020', 
+  '1-January-2025', 
+  'BLACK HOLE SIMULATOR PROJECT', 
 		'Y',
 		(select ref(s) from PDIS s where s.NIF='11111111B'),
-        refLResearch_va((select ref(r1) from LResearches r1 where r1.name='SOFTWARE ENGINEERING'),
-		             (select ref(r2) from LResearches r2 where r2.name='ASTROPHYSICS')))                     
-        )
+  refLResearch_va((select ref(r1) from LResearches r1 where r1.name='SOFTWARE ENGINEERING'),
+		       (select ref(r2) from LResearches r2 where r2.name='ASTROPHYSICS')))         
+  )
 WHERE businessname like 'IBM';*/
 
 INSERT INTO TABLE (SELECT c.hasAgreements FROM companies c WHERE c.businessname like 'IBM') 
 VALUES (AgreementInt_ob (
-        '1-January-2020', 
-        '1-January-2025', 
-        fullname((select ref(r1) from PDIS r1 where r1.NIF='11111111B')
-        ))
+  '1-January-2020', 
+  '1-January-2025', 
+  fullname((select ref(r1) from PDIS r1 where r1.NIF='11111111B')
+  ))
 );
 
 
