fa64ec2a5bf3e71dddf27b294cf9e93f8efc29cc
jbericat <jbericat@uoc.edu>
2020-10-18 21:25:23 +0200

    v13
    added "get_student_fullname()" type object method

--- third-exercise/B_type-method-implementation.sql
+++ third-exercise/B_type-method-implementation.sql
@@ -34,7 +34,7 @@
  #                                                                              #
  ##############################################################################*/
 
-CREATE TYPE vfinalPro AS VARRAY(5) OF VARCHAR(150);
+CREATE OR REPLACE TYPE vfinalPro AS VARRAY(5) OF VARCHAR(150);
 /
 
 CREATE OR REPLACE TYPE projstudent AS OBJECT(
@@ -42,8 +42,8 @@ CREATE OR REPLACE TYPE projstudent AS OBJECT(
   stName VARCHAR(200), 
   stSurname VARCHAR(200), 
   projectPref vfinalPro,
-  --MEMBER PROCEDURE display_top_project (SELF IN OUT NOCOPY projstudent),
-  MAP MEMBER FUNCTION get_top_project RETURN VARCHAR
+  MAP MEMBER FUNCTION get_top_project RETURN VARCHAR,
+  MEMBER FUNCTION get_student_fullname RETURN VARCHAR
 );
 /
 
@@ -51,35 +51,22 @@ CREATE TABLE tbStudent OF projstudent (unicode PRIMARY KEY);
 /
 ------------------------------------------------------------------------------------
 
-/* V1 */
-
-/*
-
 CREATE OR REPLACE TYPE BODY projstudent AS 
-  MAP MEMBER FUNCTION get_top_project RETURN VARCHAR IS 
-   BEGIN
-     RETURN projectPref(5); --DE MOMENT SUPOSEM QUE SEMPRE HI HAURÀ 5 PROPOSTES DE TREBALL FINAL. SI EM DONA TEMPS IMPLEMENTARÉ UN LOOP QUE VAGI COMPROVANT, DES DE LA DARRERA FINS LA PRIMERA POSICIÓ DEL VARRAY, SI AQUEST CONTÉ UN VALOR NUL. SI = NUL; INDEX--; SINO RETURN PROJECTPREF[INDEX]. SEMPRE SUPOSAREM QUE AL MENYS HI HA UNA PROPOSTA DE TREBALL 
-   END;
-END;
-/
-
-*/
-
-/* V2 */
-
-CREATE OR REPLACE TYPE BODY projstudent AS 
-  MAP MEMBER FUNCTION get_top_project RETURN VARCHAR IS    
-    --i INT := 0;
-  BEGIN
-    <<bucle>>
-    FOR i IN REVERSE 1..5 
-    LOOP
-      IF ( projectPref(i) IS NOT NULL ) THEN
-        RETURN projectPref(i);
-      END IF;
-    END LOOP bucle;
-    RETURN 'NO RECORDS FOUND';
-  END get_top_project;
+    MAP MEMBER FUNCTION get_top_project RETURN VARCHAR IS    
+      BEGIN
+        <<bucle>>
+        FOR i IN REVERSE 1..5 
+        LOOP
+          IF ( projectPref(i) IS NOT NULL ) THEN
+            RETURN projectPref(i);
+          END IF;
+        END LOOP bucle;
+        RETURN 'NO RECORDS FOUND';
+      END get_top_project;
+    MEMBER FUNCTION get_student_fullname RETURN VARCHAR IS
+      BEGIN
+        RETURN (stName || ' ' || StSurname);
+      END get_student_fullname;
 END;
 /
 
@@ -90,55 +77,31 @@ INSERT INTO tbstudent VALUES (
  111111,
  'Freddie',
  'Mercury',
- vfinalPro('treb_pr5','treb_pr4','treb_pr3','treb_pr2','treb_pr1')
+ vfinalPro('TFG prioritat 5','TFG prioritat 4','TFG prioritat 3','TFG prioritat 2','TFG prioritat 1')
 );
  
 INSERT INTO tbstudent VALUES (
  222222,
  'Jimmy',
  'Hendrix',
- vfinalPro('treb_pr3','treb_pr2','treb_pr1','','')
+ vfinalPro('TFG prioritat 3','TFG prioritat 2','TFG prioritat 1','','')
 );
 
 ------------------------------------------------------------------------------------
 
 
-
---SELECT ts.get_top_project() from tbStudent ts;
-
-------------------------------------------------------------------------------------
-
 CREATE OR REPLACE TYPE projstudent_vt AS OBJECT (
   uniCode INTEGER,
-  stName VARCHAR(200),
+  stFullName VARCHAR(200),
   topProject VARCHAR(150)
 );
 /
 
 CREATE OR REPLACE VIEW
 TopProjects_view OF projstudent_vt WITH OBJECT OID(uniCode) AS
-  SELECT s.uniCode, s.stName, s.get_top_project()
+  SELECT s.uniCode, s.get_student_fullname(), s.get_top_project()
   FROM tbstudent s
 ;
 /
 
 SELECT * FROM topProjects_view;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
